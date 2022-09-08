219b3cccbb29ec3632b9e53c36589fb730429247
ZokuRush <67478346+ZokuRush@users.noreply.github.com>
2022-09-07 18:05:42 -0600

    Add files via upload

---
+++ client/cl_main.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function() 	
+    while true do 		
+        Citizen.Wait(0) 		
+        if IsControlPressed(1, 73) then 		
+            SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
+        end 	
+    end 
+end)
