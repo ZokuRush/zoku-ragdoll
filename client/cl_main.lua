ef493e12cc1d052a60b136240d5a3d0522972063
ZokuRush <67478346+ZokuRush@users.noreply.github.com>
2022-09-08 17:49:10 -0600

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,7 +1,9 @@
+local ragdoll = false
+
 CreateThread(function() 	
     while true do 		
         Wait(0) 		
-        if IsControlPressed(1, 74) then 		
+        if IsControlJustReleased(0, Config.Ragdoll) then	
             SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
         end 	
     end 
