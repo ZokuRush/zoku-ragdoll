30e53efd28a9df7fcbfc585e8e446486d413cb3d
ZokuRush <67478346+ZokuRush@users.noreply.github.com>
2022-09-08 18:41:22 -0600

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function() 	
     while true do 		
         Wait(0) 		
-        if IsControlJustReleased(0, Config.Ragdoll) then	
+        if IsControlPressed(0, Config.Ragdoll) then	
             SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
         end 	
     end 
